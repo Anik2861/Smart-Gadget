ee9a37d6d502d852bf6382aea28b1218f895398d
Md Anik Hossain <anikh499@gmail.com>
2022-05-09 18:37:45 +0600

    done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(cors())
 // app.use(express())
 
-// Middlewares
+// Middlewares---
 const corsConfig = {
     origin: ['https://smart-gadget-warehouse.web.app/'],
     credentials: true,
